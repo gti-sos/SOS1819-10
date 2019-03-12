17f7dcd5291702a756215b9dbecc4deee5d5e34a
sos1819jcgp <josecapg@outlook.es>
2019-03-12 08:06:10 +0000

    commit of public folder and index.html

---
+++ index.js
@@ -0,0 +1,16 @@
+var express= require("express");
+
+var app= express();
+
+var port = process.env.PORT || 8080;
+
+app.use("/",
+        express.static(__dirname+"/public"));
+
+app.get("/time", (request, response) =>{
+    response.send(new Date());
+});
+
+app.listen(port, () =>{
+    console.log("magic is happening"+port);
+});
