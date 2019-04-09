6da9a92d7e5b64ba0e482e73378521afe0008e4e
sos1819jcgp <josecapg@outlook.es>
2019-04-09 17:14:31 +0000

    added frontend jc

---
+++ public/e-cars-statics/js/main-ctrl.js
@@ -0,0 +1,56 @@
+/* global angular */
+
+var app = angular.module("MiniPostmanApp");
+
+app.controller("MainCtrl", ["$scope","$http", function ($scope,$http){
+                console.log("Modular MainCtrl initialized");
+                $scope.url = "/api/v1/e-cars-statics";
+                
+                // GET
+                
+                $scope.send = function(){
+                    $http.get($scope.url).then(function (response){
+                        $scope.status = response.status;
+                        $scope.data = JSON.stringify(response.data,null,2);
+                    }, function (error){
+                        $scope.status = error.status;
+                        $scope.data = "";
+                    });
+                };
+                
+                // POST
+                
+                $scope.post = function(){
+                    $http.post($scope.url,$scope.body).then(function (response){
+                        $scope.status = response.status;
+                        $scope.data = "";
+                    }, function (error){
+                        $scope.status = error.status;
+                        $scope.data = "";
+                    });
+                };
+                
+                // PUT
+                
+                $scope.put = function(){
+                    $http.put($scope.url,$scope.body).then(function (response){
+                        $scope.status = response.status;
+                        $scope.data = "";
+                    }, function (error){
+                        $scope.status = error.status;
+                        $scope.data = "";
+                    });
+                };
+                
+                // DELETE
+                
+                $scope.delete = function(){
+                    $http.delete($scope.url).then(function(response){
+                        $scope.status = response.status;
+                        $scope.data = "";
+                    }, function (error){
+                        $scope.status = error.status;
+                        $scope.data = "";
+                    });
+                };
+}]);
