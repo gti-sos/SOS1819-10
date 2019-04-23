e09a4d4babbeb33609ed3249b5c9492614304274
sos1819jcgp <josecapg@outlook.es>
2019-04-23 18:07:51 +0000

    add features frontend

--- public/e-car-statics/main-ctrl.js
+++ public/e-car-statics/main-ctrl.js
@@ -2,6 +2,81 @@
 
 var app = angular.module("MiniPostmanApp");
 
+app.controller("MainCtrl", ["$scope", "$http", function($scope, $http) {
+
+    console.log("MainCtrl ready");
+
+    $scope.url = "/api/v1/e-car-statics";
+
+    function refresh() {
+
+        $http.get($scope.url).then(function(response) {
+            console.log("Datos recibidos "+JSON.stringify(response.data,null,2));
+            $scope.ecarstatics = response.data;
+
+        });
+    };
+        /* El añadir un nuevo campo funciona */
+        $scope.addNewCarStatics = function() {
+
+            var newStatic = $scope.newCarStatic;
+            console.log("añadiendo una nueva estadística "+ JSON.stringify(newStatic, null, 2));
+
+            $http.post($scope.url, newStatic).then(function(response) {
+                console.log("Creado correctamente!");
+                refresh();
+
+            });
+        };
+
+        $scope.updateData = function(country, year) {
+
+            var updateData = $scope.updateData;
+
+            $http.put($scope.url + "/" + country + "/" + year, updateData).then(function(response) {
+
+                refresh();
+
+            });
+        };
+        /* Los delete funcionan */
+        $scope.deleteCarStatics = function(country, year) {
+
+            $http.delete($scope.url+"/"+country+"/"+year).then(function(response) {
+                console.log("Deleting field with country "+country+ " and year "+year);
+                refresh();
+
+            });
+        };
+
+        $scope.deleteAllData = function() {
+
+            $http.delete($scope.url).then(function(response) {
+
+                refresh();
+            });
+        };
+        
+        /* Realizar búsqueda*/
+        $scope.buscarDesdeHasta = function(){
+            console.log("buscando...");
+            $http({
+                url: $scope.url, 
+                method: "GET",
+                params: {from: $scope.from, to: $scope.to}
+            }).then(function (response){
+                $scope.ecarstatics= response.data;
+                console.log("Búsqueda realizada "+JSON.stringify(response.data,null,2));
+            });
+                
+        };
+        
+    refresh();
+}]);
+
+/*
+var app = angular.module("MiniPostmanApp");
+
 app.controller("MainCtrl", ["$scope","$http", function ($scope,$http){
                 console.log("MainCtrl initialized");
                 $scope.url = "/api/v1/e-car-statics";
@@ -47,3 +122,5 @@ app.controller("MainCtrl", ["$scope","$http", function ($scope,$http){
                     });
                 };
 }]);
+
+*/
