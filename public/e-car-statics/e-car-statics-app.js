84676495b020038a3df067d392eb57b55262a704
sos1819jcgp <josecapg@outlook.es>
2019-05-13 18:18:16 +0000

    edit

---
+++ public/e-car-statics/e-car-statics-app.js
@@ -0,0 +1,16 @@
+ /* global angular*/
+
+ var app = angular.module("Electric-Car-StaticsApp", ["ngRoute"]).config(function($routeProvider) {
+
+     $routeProvider.when("/", {
+
+         controller: "ListCtrl",
+         templateUrl: "list.html"
+     }).when("/edit/:country/:year", {
+
+         controller: "EditCtrl",
+         templateUrl: "edit.html"
+     });
+ });
+
+ console.log("App init");
