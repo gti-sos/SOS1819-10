429cd1b3acf75f73d8b9635839b06ac98228254e
sos1819jcgp <josecapg@outlook.es>
2019-05-14 20:46:43 +0000

    e2e

--- tests-issue/test.js
+++ tests-issue/test.js
@@ -2,12 +2,12 @@ exports.config = {
   
   seleniumAddress: "http://localhost:4444/wd/hub",
   chromeOnly: true,
-  specs: ["Frontend/TC01-listEmpty.js",
-          "Frontend/TC02-loadList.js",
-          "Frontend/TC03-createIssue.js",
-          "Frontend/TC04-deleteIssue.js",
-          "TC05-loadDataecarstatics.js",
-		      "TC06-createDataecarstatics.js",
-		      "TC07-removeDataecarstatics.js"
+  specs: ["e2e/TC01-listEmpty.js",
+          "e2e/TC02-loadList.js",
+          "e2e/TC03-createIssue.js",
+          "e2e/TC04-deleteIssue.js",
+          "e2e/TC05-loadDataecarstatics.js",
+		      "e2e/TC06-createDataecarstatics.js",
+		      "e2e/TC07-removeDataecarstatics.js"
   ]
 };
