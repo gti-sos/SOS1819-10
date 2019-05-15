ad847276b45f9d62d612c2798eb1acfca8a3deba
sos1819fpc <fraparcas@alum.us.es>
2019-05-15 07:00:23 +0000

    changes

---
+++ tests-ecarstatics/test.js
@@ -0,0 +1,9 @@
+exports.config = {
+
+  seleniumAddress: "http://localhost:4444/wd/hub",
+  chromeOnly: true,
+  specs: ["TC01-createDataecarstatics.js",
+    "TC02-loadDataecarstatics.js",
+    "TC03-removeDataecarstatics.js"
+  ]
+};
