db995366383747b05989fe85359c653dc87bb436
sos1819fpc <fraparcas@alum.us.es>
2019-05-30 15:12:01 +0000

    save

--- public/issue-dioxid/dygraphs-ctrl.js
+++ public/issue-dioxid/dygraphs-ctrl.js
@@ -1,16 +1,40 @@
-/*global angular*/
+/*global angular, Dygraph*/
 
 var app = angular.module("EnvironmentApp");
 
 app.controller("GraphCrtl", ["$scope", "$http", "$location", "$routeParams", function($scope, $http, $location, $routeParams) {
-    
+
     console.log("GraphCtrl ready");
 
-    $scope.url = "/api/v2/issue-dioxid";
-    
-    $http.get($scope.url).then(function(response){
+    var url = "/api/v2/issue-dioxid";
+
+    $http.get(url).then(function(response) {
+
+        var emisiones = response.data;
+
+        var datosGraficos = [];
         
-        datos = response.data;
-    });
+        emisiones.forEach(function(i){
+            
+            datosGraficos.push([parseInt(i.year), parseInt(i.issue_metric_ton)]);
+
+        });
+        
+        $scope.graphiss = new Dygraph(
+
+            document.getElementById("graphiss"),
+
+            datosGraficos,
+
+            {
+                labels: ["x-coord", "issue_metric_ton"],
+            }
+        );
+
+    }, function(error) {
     
+        console.log("Error al recoger datos");
+
+    });
+
 }]);
