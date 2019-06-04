f871425e81430d56d72d39f3ff891562d13179ff
sos1819jcgp <josecapg@outlook.es>
2019-06-04 08:01:02 +0000

    a

--- public/e-car-statics/integrations/uefa-country-rankings-ctrl.js
+++ public/e-car-statics/integrations/uefa-country-rankings-ctrl.js
@@ -7,20 +7,76 @@ app.controller("uefaCountry", ["$scope", "$http", function($scope, $http) {
 
     console.log("List Uefa Controller initialized.");
 
-    var API = "https://sos1819-06.herokuapp.com/api/v1/uefa-country-rankings";
+    var API1 = "/api/v1/e-car-statics";
+    var API2 = "https://sos1819-06.herokuapp.com/api/v1/uefa-country-rankings";
     refresh();
 
     function refresh() {
 
-        console.log("Requesting expenses to <" + API + ">...");
-        $http
-            .get(API)
-            .then(function(response) {
+        $http.get(API1).then(function(responseCar) {
+            $http.get(API2).then(function(responseCountry) {
+                
+                $scope.uefacountries=responseCountry.data;
+                var datos = [];
 
-                console.log("Data received:" + JSON.stringify(response.data, null, 2));
+                for (var i in responseCar.data) {
+                    var dato = {
+                        name: "Country: " + responseCar.data.map(function(d) { return d["country"] + " " + d["year"] })[i],
+                        y: responseCar.data.map(function(d) { return d["existsVehicles"] })[i]
+                    };
+                    datos.push(dato);
+                }
 
-                $scope.uefacountries = response.data;
+                for (var i in responseCountry.data) {
+                    var dato1 = {
+                        name: "Country: " + responseCountry.data.map(function(d) { return d["country"] + " " + d["season"] })[i],
+                        y: responseCountry.data.map(function(d) { return d["points"] })[i]
+                    };
+                    datos.push(dato1);
+                }
+                console.log(datos)
+
+                Highcharts.chart('#integration1', {
+                    chart: {
+                        plotBackgroundColor: null,
+                        plotBorderWidth: 0,
+                        plotShadow: false
+                    },
+                    title: {
+                        text: '<br>Use of electric car and points in Uefa<br>',
+                        align: 'center',
+                        verticalAlign: 'middle',
+                        y: 40
+                    },
+                    tooltip: {
+                        pointFormat: '{series.name}: <b>{point.y}</b>'
+                    },
+                    plotOptions: {
+                        pie: {
+                            dataLabels: {
+                                enabled: true,
+                                distance: -50,
+                                style: {
+                                    fontWeight: 'bold',
+                                    color: 'white'
+                                }
+                            },
+                            startAngle: -90,
+                            endAngle: 90,
+                            center: ['50%', '50%'],
+                            size: '110%'
+                        }
+                    },
+                    series: [{
+                        type: 'pie',
+                        name: 'Data',
+                        innerSize: '50%',
+                        data: datos
+                    }]
+                });
+
             });
+        });
     }
 
 }]);
